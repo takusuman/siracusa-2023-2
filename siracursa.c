3c65a24c0234e2c93fe48d3f3ebc7b0e0d08dec4
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2023-10-29 13:30:04 -0300

    Breve correção no código.

--- siracursa.c
+++ siracursa.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void main(void) {
-	long int n;
+	signed long int n;
 	
 	puts("Conjectura de Collatz, vulgo Problema de Siracusa");
 	puts("Curso de Engenharia da Computação pela Universidade Norte do Paraná, 2023/2");
@@ -12,6 +12,17 @@ void main(void) {
 	fputs("Insira um número inteiro: ", stdout);
 	scanf("%ld", &n);
 	
+	// No código que enviei à faculdade, eu esqueci de adicionar isso.
+	// Por favor, não esqueçam disso, um número negativo ou igual a zero
+	// quebra o algoritmo inteiro.
+	// Como o foco desse código estar público não é apenas mostrar o que eu
+	// enviei, mas também uma solução ao problema, é bom que ele esteja
+	// correto, mesmo que tarde.
+	if ( n < 1 ) {
+		fprintf(stderr, "O número \"n\" é menor do que 1 (%ld).\n", n);
+		exit(1);
+	}
+
 	for (;;) {
 		if (n != 1) {
 			if ( (n % 2) == 0 ) {
